7da689b53ddbfcc8800903b4a4396148e44e8e87
robert <00857002@email.ntou.edu.tw>
2022-05-05 18:36:05 +0800

    Initial Commit

---
+++ candy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  candy
+//
+//  Created by 賴冠宏 on 2022/5/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ candy/candyApp.swift
@@ -0,0 +1,17 @@
+//
+//  candyApp.swift
+//  candy
+//
+//  Created by 賴冠宏 on 2022/5/5.
+//
+
+import SwiftUI
+
+@main
+struct candyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
